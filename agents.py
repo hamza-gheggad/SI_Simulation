539b1b69a0765981d3dcc2dddbfa951e78002777
hamza-gheggad <41976870+hamza-gheggad@users.noreply.github.com>
2020-02-21 03:10:17 +0100

    Add files via upload

--- agents.py
+++ agents.py
@@ -1,52 +1,62 @@
+class subnet:
+    def __init__(self, name, components):
+        self.name = name
+        self.components = components
+
+    def add_node(self, new_node):
+        self.components.append(new_node)
+
+
 class Machine:
-    def __init__(self, name, os, IP_address, installed_software):
+    def __init__(self, name, os, IP_address, installed_software, subnet=subnet("NULL", [])):
         self.name = name
         self.os = os
         self.IP_address = IP_address
         self.installed_software = installed_software
+        self.subnet = subnet
 
     def turn_on(self):
-        pass
+        print("{} est en marche.".format(self.name))
 
     def turn_off(self):
-        pass
+        print("{} est arretée.".format(self.name))
 
 
 class Victim_Machine(Machine):
-    def __init__(self, name, os, IP_address, installed_software, vulnerabilities, defense_actions):
+    def __init__(self, name, os, IP_address, installed_software, vulnerabilities, defense_actions=[], subnet=subnet("NULL", [])):
         self.name = name
         self.os = os
         self.IP_address = IP_address
         self.installed_software = installed_software
-        self.vulnerabilities
+        self.vulnerabilities = vulnerabilities
         self.defense_actions = defense_actions
+        self.subnet = subnet
 
 
 class Attacking_Machine(Machine):
-    def __init__(self, name, os, IP_address, installed_software, attack_actions):
+    def __init__(self, name, os, IP_address, installed_software, attack_actions, subnet=subnet("NULL", [])):
         self.name = name
         self.os = os
         self.IP_address = IP_address
         self.installed_software = installed_software
         self.attack_actions = attack_actions
+        self.subnet = subnet
 
 
-class Victim_Machine(Machine):
-    def __init__(self, name, os, IP_address, installed_software, rules):
+class parfeu(Machine):
+    def __init__(self, name, os, IP_address, installed_software, rules, subnet=subnet("NULL", [])):
         self.name = name
         self.os = os
         self.IP_address = IP_address
         self.installed_software = installed_software
         self.rules = rules
+        self.subnet = subnet
 
-    def add_rule(self):
-        pass
-
-    def set_rule(self):
-        pass
+    def add_rule(self, rule):
+        return "La règle {} est ajoutée au parfeu {}.".format(rule, self.name)
 
     def remove_rule(self):
-        pass
+        return "La règle {} est supprimée du parfeu {}.".format(rule, self.name)
 
 
 class Server(Victim_Machine):
@@ -65,7 +75,7 @@ class Client(Victim_Machine):
         self.installed_software = installed_software
 
 
-class Web_server(Server):
+class web_server(Server):
     def __init__(self, name, os, IP_address, installed_software):
         self.name = name
         self.os = os
@@ -73,7 +83,7 @@ class Web_server(Server):
         self.installed_software = installed_software
 
 
-class Mail_server(Server):
+class mail_server(Server):
     def __init__(self, name, os, IP_address, installed_software):
         self.name = name
         self.os = os
@@ -87,7 +97,7 @@ class Utilisateur:
         self.Machine = Machine
 
     def connect_to(self, Machine):
-        pass
+        print("L'utilisateur {} est connecté à {}.".format(self.name, Machine.name))
 
 
 class Victime(Utilisateur):
@@ -100,18 +110,18 @@ class Victime(Utilisateur):
 
 
 class Attaquant(Utilisateur):
-    def __init__(self, name, Victim_Machine):
+    def __init__(self, name, Attacking_Machine):
         self.name = name
-        self.Victim_Machine = Victim_Machine
+        self.Attacking_Machine = Attacking_Machine
 
-    def execAttack(self):
-        pass
+    def execAttack(self, attaque, Destination_Machine):
+        return "L'attaque {} est exécutée sur {}.".format(attaque, Destination_Machine)
 
-
-class Subnet:
-    def __init__(self, name, components):
-        self.name = name
-        self.components = components
+    def list_software(self, Destination_Machine):
+        for node in self.Attacking_Machine.subnet.components:
+            if node.name == Destination_Machine:
+                for software in node.installed_software:
+                    print(software.name)
 
 
 class Software:
@@ -133,7 +143,7 @@ class Router:
 
 class NIDS:
     def __init__(self, nom):
-        self.nom = nom
+        self.name = name
 
     def alert(self):
         pass
@@ -141,7 +151,7 @@ class NIDS:
 
 class HIDS:
     def __init__(self, nom):
-        self.nom = nom
+        self.name = name
 
     def alert(self):
         pass
